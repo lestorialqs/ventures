05ada1c8c743b002585d7b1df2188ef75cd92e34
Henry Quispe Sutta <Henryqs>
2024-11-19 14:28:11 -0500

    first

---
+++ src/test/java/com/dbp/ventures/VenturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbp.ventures;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenturesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
