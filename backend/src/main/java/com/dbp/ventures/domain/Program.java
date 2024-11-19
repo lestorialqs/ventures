aa48807e8fffc66d933142db3c3a2f05625513df
Henry Quispe Sutta <Henryqs>
2024-11-19 14:47:41 -0500

    v2 back

--- backend/src/main/java/com/dbp/ventures/domain/Program.java
+++ backend/src/main/java/com/dbp/ventures/domain/Program.java
@@ -1,12 +1,8 @@
 package com.dbp.ventures.domain;
 
+
 import jakarta.persistence.Entity;
-import org.springframework.data.annotation.Id;
 
 @Entity
-
-public class Utec {
-
-
-
+public class Program {
 }
