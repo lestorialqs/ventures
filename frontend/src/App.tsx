cdac11971f808543e173a3ab177c7ac2e1283b91
seo-onion <sebastianhernandezminano@gmail.com>
2024-11-19 15:05:37 -0500

    v10

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./App.css";
+import DeleteProgram from "./pages/deleteService";
+import EditProgram from "./pages/putService";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Pagina principal</h1>
+  },
+  {
+    path: "/delete",
+    element: <DeleteProgram />,
+  },
+  {
+    path: "/edit",
+    element: <EditProgram />,
+  },
+]);
 
 function App() {
-
   return (
     <>
-
+      <RouterProvider router={router} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
